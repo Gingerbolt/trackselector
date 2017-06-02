2632dba67a7a3b456917a66fd829e535afee757c
Steve Spitz <steve@spitz.com>
2017-06-02 15:29:08 -0700

    add neutered but functional logic; made system work in rudimentary fashion

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ $( document ).ready(function() {
   var ruby = "Ruby/Rails";
   var php = "PHP/Drupal";
   var java = "Java/Android";
-  var css = "CSS/Design";
+  var css = "CSS/Design or ";
   var cnet = "C#/.NET";
-  var intro = "intro to programming"
+  var intro = "Intro to programming";
     $("#submitbtn").click(function(){
       console.log("clicked")
 
@@ -21,26 +21,30 @@ $( document ).ready(function() {
       var popularity = parseInt($("select#popularinput").val());
       var operating = parseInt($("select#osinput").val());
       var size = parseInt($("select#companyinput").val());
+      var experience =
+      parseInt($("select#experienceinput").val());
 
-      console.log(frontback);
-      console.log(webbiz);
-      console.log(popularity);
-      console.log(operating);
-      console.log(size);
+      console.log(experience);
+
       //  separate design condition
-      if (frontback === 1) {
-        $("#suggestion1").text(css)
-      };
-      if (size === 3 && frontback === 2 && operating === 1 || operating === 3 ) {
+      if (size === 3){
         $("#suggestion2").text(cnet)
-      } else if (size === 2 && frontback === 2){
+      };
+      if (size === 2){
         $("#suggestion2").text(java)
-      } else if (size === 3 &&& frontback === 2) {
-        $("suggestion2").text(ruby)
-      } else if (frontback === 2 && popularity === 1 && size >= 2){
+      };
+      if (size === 1){
+        $("#suggestion2").text(ruby)
+      };
+      if (popularity === 1 && size >= 2){
         $("suggestion2").text(php)
-      } else {
-        $("suggestion2").text(intro)
+      };
+
+      if (frontback === 1){
+        $("#suggestion2").prepend(css)
+      };
+      if (experience === 1){
+        $("#suggestion2").text(intro)
       };
       $("#namesuggest").text(namesuggestion);
       $("#answer").show();
