4dd63b66ed969c24e031173e159459d75728ce7b
Steve Spitz <steve@spitz.com>
2017-06-02 14:25:36 -0700

    strip logic down to bare functional essentials

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
   console.log("ready");
+  // Output result variables
+  var ruby = "Ruby/Rails";
+  var php = "PHP/Drupal";
+  var java = "Java/Android";
+  var css = "CSS/Design";
+  var cnet = "C#/.NET";
+  var intro = "intro to programming"
     $("#submitbtn").click(function(){
       console.log("clicked")
 
@@ -15,14 +22,6 @@ $( document ).ready(function() {
       var operating = parseInt($("select#osinput").val());
       var size = parseInt($("select#companyinput").val());
 
-      // Output result variables
-      var ruby = "Ruby/Rails";
-      var php = "PHP/Drupal";
-      var java = "Java/Android";
-      var css = "CSS/Design";
-      var cnet = "C#/.NET";
-      var intro = "intro to programming"
-
       console.log(frontback);
       console.log(webbiz);
       console.log(popularity);
@@ -32,13 +31,13 @@ $( document ).ready(function() {
       if (frontback === 1) {
         $("#suggestion1").text(css)
       };
-      if (size >= 2 && frontback === 2 && webbiz === 2 && operating === 1 || operating === 3 ) {
+      if (size === 3 && frontback === 2 && operating === 1 || operating === 3 ) {
         $("#suggestion2").text(cnet)
-      } else if (size <= 2 && operating === 3 && webbiz === 1 && popularity === 1){
+      } else if (size === 2 && frontback === 2){
         $("#suggestion2").text(java)
-      } else if (webbiz === 1 && popularity === 2 && operating === 2 && frontback === 2) {
+      } else if (size === 3 &&& frontback === 2) {
         $("suggestion2").text(ruby)
-      } else if (frontback === 2 && popularity === 1 && webbiz === 1){
+      } else if (frontback === 2 && popularity === 1 && size >= 2){
         $("suggestion2").text(php)
       } else {
         $("suggestion2").text(intro)
